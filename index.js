c39d2a10b4e5f20a45c029403c0862ee0b423bb4
Benis Tambe <will.jenifer@yahoo.com>
2021-08-13 14:16:53 -0400

    commit tree 1e47deb5c1ffa0ab125d3abd8afb5277f48dc03b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+let findMatching = (driversArr, str) => {
+  const matched = driversArr.filter(d => d.toLowerCase() === str.toLowerCase());
+  return matched;
+}
+
+let fuzzyMatch = (driversArr, str) => {
+  const matched = driversArr.filter(d => d.startsWith(str));
+  return matched;
+}
+
+let matchName = (driversArr, str) => {
+  const matched = driversArr.filter(d => d.name.toLowerCase() === str.toLowerCase());
+  return matched;
+}
